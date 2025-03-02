1216551ec82ef86d71c11a263bc96b6ff03e35b0
ppranay-20 <pranay@omaratechnologies.com>
2025-03-02 23:25:41 +0530

    changed env

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: "dotenv");
+  await dotenv.load(fileName: "assets/dotenv");
   runApp(const MyApp());
 }
 
